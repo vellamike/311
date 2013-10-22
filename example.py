901b33e3707c57c2d9080891b267ec59b6eed6f7
Mike Vella <vellamike@gmail.com>
2013-10-22 14:56:26 +0100

    added some more interesting plots

--- example.py
+++ example.py
@@ -12,13 +12,29 @@ import learn
 import numpy as np
 import math
 
+def plot(x,y,xlabel,ylabel):
+    plt.plot(x,y,'x')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
+
+
 d = learn.load_data("data/train_four.csv")
 
 chars_in_description = map(len,d.description)
+chars_in_summary = map(len,d.summary)
 num_votes = d.num_votes
 num_views = d.num_views
+latitude = d.latitude
+longitude = d.longitude
 
-plt.plot(chars_in_description,num_views,'x')
-plt.xlabel('characters in description')
-plt.ylabel('number of views')
-plt.show()
+plot(chars_in_summary,num_views,'characters in summary','number of views')
+plot(chars_in_description,num_views,'characters in description','number of views')
+plot(latitude,num_views,'latitude','number of views')
+plot(longitude,num_views,'longitude','number of views')
+
+log_num_views = map(math.log,num_views + 1)
+plot(chars_in_summary,log_num_views,'characters in summary','log + 1 of number of views')
+plot(chars_in_description,log_num_views,'characters in description','log + 1 of number of views')
+plot(latitude,log_num_views,'latitude','log + 1 of number of views')
+plot(longitude,log_num_views,'longitude','log + 1 of number of views')

e886f08cb8c88f41eabdbe1fd94f8bf4fd884919
Ben Derrett <ben.derrett@gmail.com>
2013-10-30 17:57:21 +0000

    Reorganized learn.py. Introduced Model class.

--- learn.py
+++ learn.py
@@ -1,5 +1,8 @@
+''' Functions to learn, train and predict. ''' 
+
+# maths
 import numpy as np
-import io
+import pandas
 
 # sklearn imports
 from sklearn import svm
@@ -10,37 +13,21 @@ from sklearn.linear_model import SGDRegressor
 import sklearn.preprocessing
 
 import pdb
-from numpy import genfromtxt
 import math
 import time
-import pandas
+import io
+
+# our code
 import city_extraction
 
 cols_to_predict = ['num_comments', 'num_views', 'num_votes']
 
-def load_data(path="data/train.csv"):
-    return pandas.read_csv(path)
-
-def train(d):
-    s_d = make_category_dict(d.source.values)
-    t_d = make_category_dict(d.tag_type.values)
-    (features, enc) = make_features(d, source_dic = s_d, tag_dic = t_d)
-    regressors = []
-    start = time.time()
-    for col_name in cols_to_predict:
-        r = SGDRegressor(loss = "squared_loss", n_iter = 10, alpha = 0, power_t
-                        = 0.1, shuffle = True)
-        r.fit(features, tog(d[col_name].values))
-        regressors.append(r)
-    print(time.time() - start)
-    # reg = ensemble.GradientBoostingRegressor(**params)
-    #features = np.column_stack((d.chars_in_description[:k],
-    #                            d.chars_in_summary[:k],
-    #                            encoded_features[:k, :]
-    #                           ))
-    return (regressors, s_d, t_d, enc)
-
-
+def load_data(training_set):
+    ''' Loads training or test data. '''
+    if training:
+        return pandas.read_csv("data/train.csv")
+    else:
+        return pandas.read_csv("data/test.csv")
 
 def tog(x):
     return np.log(x + 1)
@@ -48,52 +35,55 @@ def tog(x):
 def untog(x):
     return np.exp(x) - 1
 
-def train_predict(d, outfile = "data/predictions.csv"):
-    (regressors, s_d, t_d, enc) = train(d)
-    training_predictions = predict(regressors, s_d, t_d, enc, training_set = True)
-    # training set prediction error
-    e1 = tog(d.num_comments.values) - tog(training_predictions[0])
-    e2 = tog(d.num_views.values) - tog(training_predictions[1])
-    e3 = tog(d.num_votes.values) - tog(training_predictions[2])
-    error = rms(np.concatenate([e1,e2,e3]))
-    print("Training set error: {0}".format(error))
-    # predict test set
-    test_predictions = predict(regressors, s_d, t_d, enc, training_set = False)
-    
-    id = ['id']
-    num_views = ['num_views']
-    num_votes = ['num_votes']
-    num_comments = ['num_comments']
-
-    ids = np.concatenate([id, d['id'].values.astype(dtype = 'S')])
-    comments = np.concatenate([num_comments, test_predictions[0]])
-    views = np.concatenate([num_views, test_predictions[1]])
-    votes = np.concatenate([num_votes, test_predictions[2]])
-
-    with open(outfile,'w') as handle:
-        for (id, comment, view, vote) in zip(ids, comments, views, votes):
-            handle.write("{0},{1},{2},{3}\n".format(id, view, vote, comment))
-    
-    return test_predictions
-
 def rms(x):
     return np.sqrt(np.sum(x**2)/len(x))
 
-def predict(regressors, s_d, t_d, enc, training_set = False):
-    if training_set:
-        data = pandas.read_csv('data/train.csv')
-    else:
-        data = pandas.read_csv('data/test.csv')
-    (test_features, enc) = make_features(data, source_dic = s_d, tag_dic = t_d,
-                                  enc = enc)
-    predictions = []
-    for r in regressors:
-        predictions.append(untog(r.predict(test_features)))
-        predictions[-1]  = np.maximum(predictions[-1], 0)
-    
-    if not training_set:
-        predictions = set_tog_means(predictions)
-    return predictions
+class Model:
+    def __init__(self, training_data = None, test_data = None):
+        if training_data is None:
+            self.tr_d = load_data(training_set = True)
+        if test_data is None:
+            self.te_d = load_data(training_set = False)
+
+    def __make_features__(self):
+        int_features = np.zeros((len(self.tr_d.id), 4))
+        int_features[:,0] = feature_to_int(self.tr_d.source.values, category_dict = self.s_d) 
+        # 9 values in training set
+        int_features[:,1] = feature_to_int(self.tr_d.tag_type.values, category_dict = self.t_d)
+        int_features[:,2] = city_feature(self.tr_d)
+        int_features[:,3] = map(int, self.tr_d.description > 0)
+        # 43 values in training set
+        if self.enc is None:
+            self.enc = sklearn.preprocessing.OneHotEncoder()
+            self.encoded_features =  self.enc.fit_transform(int_features).todense()
+        else:
+            self.encoded_features = self.enc.transform(int_features).todense()
+
+    def train(self):
+        self.s_d = make_category_dict(d.source.values)
+        self.t_d = make_category_dict(d.tag_type.values)
+        self.make_features()
+        self.regressors = []
+        start = time.time()
+        for col_name in cols_to_predict:
+            r = SGDRegressor(loss = "squared_loss", n_iter = 10, alpha = 0, power_t
+                            = 0.1, shuffle = True)
+            r.fit(features, tog(self.tr_d[col_name].values))
+            self.regressors.append(r)
+        print(time.time() - start)
+
+    def predict(regressors, s_d, t_d, enc, training_set = False):
+        data = self.tr_d if training_set else self.te_d
+        (test_features, enc) = make_features(data, source_dic = s_d, tag_dic = t_d,
+                                      enc = enc)
+        predictions = []
+        for r in regressors:
+            predictions.append(untog(r.predict(test_features)))
+            predictions[-1]  = np.maximum(predictions[-1], 0)
+        
+        if not training_set:
+            predictions.correct_means()
+        return predictions
 
 class Predictions:
     ''' Represents a set of test predictions. '''
@@ -102,22 +92,24 @@ class Predictions:
         self.view_p = view_p
         self.vote_p = vote_p
 
-    def set_tog_means(self):
-        ''' Assume predictions is a 3 tuple of arrays: first comment predictions,
-        then views, then votes.'''
+    def training_set_error(training_data):
+        d = training_data 
+        e1 = tog(d.num_comments.values) - tog(self.comment_p)
+        e2 = tog(d.num_views.values) - tog(self.view_p)
+        e3 = tog(d.num_votes.values) - tog(self.vote_p)
+        error = rms(np.concatenate([e1,e2,e3]))
+        return error
+
+    def correct_means(self):
         log_mean_comments = 0.02665824
         log_mean_views = 0.41264568
         log_mean_votes = 0.80850881
-        means = (log_mean_comments,
-                 log_mean_views,
-                 log_mean_votes)
+        self.comment_p = set_tog_mean(self.comment_p, log_mean_comments) 
+        self.view_p = set_tog_mean(self.view_p, log_mean_views)
+        self.vote_p = set_tog_mean(self.vote_p, log_mean_votes)
+        self.corrected = True
 
-        scaled_predictions = []
-        for (m, p) in zip(means, predictions):
-            scaled_predictions.append(set_tog_mean(p, m))
-        return scaled_predictions
-
-    def set_tog_mean(arr, m):
+    def __set_tog_mean__(arr, m):
         scale_factor_lb = 0
         scale_factor_ub = 2
         while scale_factor_ub - scale_factor_lb > 10**(-7):
@@ -129,6 +121,23 @@ class Predictions:
         print('''Correction factor: {0}.'''.format(guess))
         return guess * arr
 
+    def write(file = "predictions.csv"):
+        assert(self.corrected)
+
+        id = ['id']
+        num_views = ['num_views']
+        num_votes = ['num_votes']
+        num_comments = ['num_comments']
+
+        ids = np.concatenate([id, d['id'].values.astype(dtype = 'S')])
+        comments = np.concatenate([num_comments, self.comment_p])
+        views = np.concatenate([num_views, self.view_p])
+        votes = np.concatenate([num_votes, self.vote_p])
+
+        with open(file,'w') as handle:
+            for (id, comment, view, vote) in zip(ids, comments, views, votes):
+                handle.write("{0},{1},{2},{3}\n".format(id, view, vote, comment))
+    
 def make_vw_training_set(d, features):
     with open("data/vowpal_training.vw","w") as handle:
         for (id, tog_views, f) in zip(d.id,
